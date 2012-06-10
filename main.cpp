297859778c09d4fb3030af021d0ddd793dbc6e96
stereomatching <luke@luke-PC.(none)>
2012-06-11 01:14:09 +0800

    add template parameters for test functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "gil_core_unit_test.hpp"
+//#include "gil_resample_unit_test.hpp"
 
 int main(int argc, char *argv[])
-{
-    test_gil_absdiff();
-    test_gil_add();
+{        
+    test_gil_absdiff<boost::gil::gray8_image_t>();
+    test_gil_add<boost::gil::gray8_image_t>();
+    //test_bilinear();
+    //test_bilinear_resample();
 
     return 0;
 }
