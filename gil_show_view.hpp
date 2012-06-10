2ffa2f5b36dd92caa80e3b168582bc875dbdeb46
stereomatching <luke@luke-PC.(none)>
2012-06-10 19:55:40 +0800

    change T to View

--- gil_show_view.hpp
+++ gil_show_view.hpp
@@ -9,15 +9,15 @@
  * @param :
  *  view : the view you want to show
  */
-template<typename T>
-void show_view(T const &view)
+template<typename View>
+void show_view(View const &view)
 {
     for(int i = 0; i != view.height(); ++i)
     {
         auto src_it = view.row_begin(i);
         for(int j = 0; j != view.width(); ++j)
         {
-            for(int c = 0; c != boost::gil::num_channels<T>::value; ++c)
+            for(int c = 0; c != boost::gil::num_channels<View>::value; ++c)
             std::cout<<"channel "<< c << " : ("<<i<<", "<<j<<") = "<<(int)src_it[j][c]<<", ";
         }
         std::cout<<std::endl;
